b3609cb798c475824eced5ae70f28e7e31c15da5
krzysztofhejna <khejna15@wp.pl>
2019-01-17 17:53:01 +0100

    Implement image hosting feature

--- modules/server.js
+++ modules/server.js
@@ -8,19 +8,23 @@ function start() {
     console.log("Zapytanie " + request.url + " odebrane.");
 
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-    case '/':
-    case '/start':
+    switch (request.url) {
+      case '/':
+      case '/start':
         handlers.welcome(request, response);
         break;
-    case '/upload':
+      case '/upload':
         handlers.upload(request, response);
         break;
-    default:
+      case '/show':
+        handlers.show(request, response);
+        break;
+      default:
         handlers.error(request, response);
+    }
   };
 
   http.createServer(onRequest).listen(9000);
-
   console.log("Uruchomiono serwer!".green);
 }
 
